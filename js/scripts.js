121cd8fb83d1bf84f4e7243dee8abe5b4704fd2d
silvermistshadow <bmead72@gmail.com>
2019-03-01 13:30:25 -0800

    keep writing html and javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
-  var getClickedItem = function(){
-    return true;
+    $("#formOne").submit(function(event){
+      event.preventDefault();
+      var inputString = $("#animalSelect").val();
 
-  };
-  
+      if (inputString == "snake") {
+        $("#snake").show();
+      }
+      else if (inputString == "lizard") {
+        $("#lizard").show();
+      }
+      else if (inputString == "turtle") {
+        $("#turtle").show();
+      }
+
+    });
+
+
 
 
 });
