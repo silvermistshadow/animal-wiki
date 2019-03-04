a06423d779aeb0d6d4df104e6948803263c2b55a
silvermistshadow <bmead72@gmail.com>
2019-03-04 09:49:18 -0800

    finalize html and javascript

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,17 @@ $(document).ready(function(){
 
       if (inputString == "snake") {
         $("#snake").show();
+        $("#lizard").hide();
+        $("#turtle").hide();
       }
       else if (inputString == "lizard") {
         $("#lizard").show();
+        $("#snake").hide();
       }
       else if (inputString == "turtle") {
         $("#turtle").show();
+        $("#snake").hide();
+        $("#lizard").hide();
       }
 
     });
